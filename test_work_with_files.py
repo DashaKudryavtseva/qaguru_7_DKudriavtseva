10c4f1e54ba272ca64440aa3ec1e4905cd225c19
Daria Kudriavtseva <situation96@mail.ru>
2023-05-06 16:10:39 +0300

    отрефакторен тест

--- test_work_with_files.py
+++ test_work_with_files.py
@@ -5,7 +5,7 @@ from openpyxl import load_workbook
 from PyPDF2 import PdfReader
 
 
-@pytest.fixture
+@pytest.fixture()
 def archiving_files():
     files_for_pack = ["file_example_XLSX_10.xlsx", "sample.pdf", "username.csv"]
     with ZipFile("Simple.zip", mode="w") as zip_file:
@@ -24,13 +24,20 @@ def xlsx_content_check(file):
     xl_file.close()
 
 
-def test_check_zip_xlsx():
+def test_check_zip_files(archiving_files):
     with ZipFile("Simple.zip") as check_zip:
         name_files = check_zip.namelist()
+
         for file in name_files:
             if ".xlsx" in file:
                 assert file == "file_example_XLSX_10.xlsx"
                 xlsx_content_check(file)
+            elif ".csv" in file:
+                assert file == "username.csv"
+                csv_content_check(file)
+            elif ".pdf" in file:
+                assert file == "sample.pdf"
+                pdf_content_check(file)
 
 
 def csv_content_check(file):
@@ -41,25 +48,10 @@ def csv_content_check(file):
         assert users[0]["Username"] == "booker12"
 
 
-def test_check_zip_csv():
-    with ZipFile("Simple.zip") as check_zip:
-        name_files = check_zip.namelist()
-        for file in name_files:
-            if ".csv" in file:
-                assert file == "username.csv"
-                csv_content_check(file)
-
-
 def pdf_content_check(file):
     reader = PdfReader(file)
     assert len(reader.pages) == 2
-    index = reader.pages[0].extract_text().find('This is a small demonstration .pdf file')
+    index = (
+        reader.pages[0].extract_text().find("This is a small demonstration .pdf file")
+    )
     assert index == 21
-
-def test_check_zip_pdf():
-    with ZipFile("Simple.zip") as check_zip:
-        name_files = check_zip.namelist()
-        for file in name_files:
-            if ".pdf" in file:
-                assert file == "sample.pdf"
-                pdf_content_check(file)
